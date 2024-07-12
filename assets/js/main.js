297cf032257cf88dcaf46adaf7cbe381f9e43fd4
Marzzio Chicana Romero <89101069+marzzioChicana@users.noreply.github.com>
2024-07-12 16:48:24 -0500

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+/*========================= MENU SHOW Y HIDDEN =========================*/
+
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
