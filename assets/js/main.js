0092836d0a98d04a5b6b145759ec5bac9b291156
Marzzio Chicana Romero <89101069+marzzioChicana@users.noreply.github.com>
2024-07-18 16:18:43 -0500

    add services, portfolio, project in mind andtestimonial

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,9 +69,54 @@ tabs.forEach(tab => {
 })
 
 /*========================= SERVICES MODAL =========================*/
+const modalViews = document.querySelectorAll('.services__modal'),
+        modalBtns = document.querySelectorAll('.services__button'),
+        modalCloses = document.querySelectorAll('.services__modal-close')
 
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
 
 /*========================= PORTFOLIO SWIPER =========================*/
-
+let swiperPortfolio = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    }
+});
 
 /*========================= TESTIMONIAL =========================*/
+var swiperTestimonial = new Swiper(".testimonial__container", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    breakpoints: {
+        568: {
+            slidesPerView: 2,
+        }
+    }
+  });
